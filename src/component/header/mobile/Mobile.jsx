c1460db42e295f1e01b87a662fd1d296ad2d229e
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-07-29 21:37:41 +0545

    updates

--- src/component/header/mobile/Mobile.jsx
+++ src/component/header/mobile/Mobile.jsx
@@ -8,24 +8,24 @@ const Mobile = ({ isOpen, setIsOpen }) => {
       </div>
       <div className="mobile-options">
         <div className="mobile-option">
-          <a href="#project">
+          <a href="#project" onClick={() => setIsOpen(false)}>
             <i className="fi-rr-edit-alt option-icon" />
             Projects
           </a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={() => setIsOpen(false)}>
           <a href="#skills">
             <i className="fi-rr-laptop option-icon" />
             Skills
           </a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={() => setIsOpen(false)}>
           <a href="#education">
             <i className="fi-rr-book-alt option-icon" />
             Education
           </a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={() => setIsOpen(false)}>
           <a href="#work">
             <i className="fi-rr-briefcase option-icon" />
             Work
