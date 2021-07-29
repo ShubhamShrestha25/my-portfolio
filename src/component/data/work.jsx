6baeac66cf64e50221e6aa01c765bc59c5ea21e6
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-07-29 22:00:52 +0545

    🔥🔥🔥 PortFolio CompleTo 🔥🔥🔥

--- src/component/data/work.jsx
+++ src/component/data/work.jsx
@@ -5,6 +5,6 @@ export const WorkData = [
     dateJoining: "No Date",
     dataEnd: "Present",
     companyLogo: "./images/notworking.jpg",
-    work: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at beatae officia quis illo iusto deleniti, debitis maxime, exercitationem ut cupiditate velit tempora mollitia obcaecati quos commodi porro nobis adipisci?",
+    work: "i am Student.I am currently studying and learning new things so currently no working experience. I am also looking for internship in Reactjs ",
   },
 ];
